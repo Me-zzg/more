9ae85f8f50544b0e5db3e3d32a0f73b7297ac2a9
zhaozhenguo <173795150@qq.com>
2019-05-15 16:09:58 +0800

    测试

---
+++ src/main/java/cn/cisdom/BootCeshiApplication.java
@@ -0,0 +1,13 @@
+package cn.cisdom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootCeshiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootCeshiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/cisdom/BootCeshiApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.cisdom;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BootCeshiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
